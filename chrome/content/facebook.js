2c24d3d12bc38b72b0d5fd243727bb205d1013ce
Ari Steinberg <ari@facebook>
2006-10-03 02:33:40 +0000

    create an xpcom module so that we can persist the current session information across multiple windows.

--- chrome/content/facebook.js
+++ chrome/content/facebook.js
@@ -1,25 +1,29 @@
-function FacebookRestClient(apiKey, secret, sessionKey) {
-    this.apiKey     = apiKey;
-    this.secret     = secret;
-    this.sessionKey = sessionKey;
+function FacebookRestClient() {
+    this.settings = Components.classes['@facebook.com/simple-service;1']
+                              .getService()
+                              .QueryInterface(Components.interfaces.fbISimpleFacebookService);
 }
 
 FacebookRestClient.prototype = {
-    generateSig: function (params) {
+    generateSig: function (params, useSessionSecret) {
         var str = '';
         params.sort();
         for (var i = 0; i < params.length; i++) {
             str += params[i];
         }
-        str += this.secret;
+        if (useSessionSecret) {
+            str += this.settings.sessionSecret;
+        } else {
+            str += this.settings.secret;
+        }
         return MD5(str);
     },
     callMethod: function (method, params, callback) {
         params.push('method=' + method);
-        params.push('session_key=' + this.sessionKey);
-        params.push('api_key=' + this.apiKey);
+        params.push('session_key=' + this.settings.sessionKey);
+        params.push('api_key=' + this.settings.apiKey);
         params.push('call_id=' + (new Date()).getTime());
-        params.push('sig=' + this.generateSig(params));
+        params.push('sig=' + this.generateSig(params, method.indexOf('facebook.auth')));
 
         var req = new XMLHttpRequest();
         req.onreadystatechange = function (event) {
@@ -43,7 +47,8 @@ FacebookRestClient.prototype = {
             req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             req.send(params.join('&'));
         } catch (e) {
-            console.log(e);
+            dump(e);
         }
     },
 }
+dump('loaded facebook.js\n');
--- chrome/content/sample.js
+++ chrome/content/sample.js
@@ -1,15 +1,56 @@
-client = new FacebookRestClient('64f19267b0e6177ea503046d801c00df', 'a8a5a57a9f9cd57473797c4612418908', '')
-var auth_token = '';
-client.callMethod('facebook.auth.createToken', [], function(req) {
-    console.log('hello');
-    console.log('req.responseText');
-    console.log(req.xmldata);
-    auth_token = req.xmldata.token;
-});
-
-function FacebookLogin() {
-    if (auth_token) {
-        window.location = 'http://api.dev005.facebook.com:4750/login.php?api_key=' + client.apiKey +
-                          '&auth_token=' + auth_token;
+var client = new FacebookRestClient();
+function startup() {
+    if (!client.settings.authToken) {
+        client.settings.apiKey = '64f19267b0e6177ea503046d801c00df';
+        client.settings.secret = 'a8a5a57a9f9cd57473797c4612418908';
+        dump('requesting token\n');
+        try {
+        client.callMethod('facebook.auth.createToken', [], function(req) {
+            dump('received token response:\n');
+            dump(req.responseText);
+            client.settings.authToken = req.xmldata.token;
+            dump('token is: '+client.settings.authToken+'\n');
+            document.getElementById('facebook-panel').label = 'logged out';
+        });
+        } catch (e) {
+            dump('exception: ' + e + '\n');
+        }
+    } else {
+        dump('already have token\n');
+        if (client.settings.sessionKey) {
+            document.getElementById('facebook-panel').label = 'logged in!';
+        } else if (client.settings.loginPending) {
+            document.getElementById('facebook-panel').label = 'When you are done, click here';
+        } else {
+            document.getElementById('facebook-panel').label = 'logged out';
+        }
     }
 }
+window.addEventListener('load', startup, false);
+
+function FacebookLogin(statusElem) {
+    if (client.settings.sessionKey) {
+        dump('already logged in!\n');
+    } else if (client.settings.loginPending) {
+        dump('FinishLogin called\n');
+        client.callMethod('facebook.auth.getSession', ['auth_token='+client.settings.authToken], function(req) {
+            dump('received session response:\n');
+            dump(req.responseText);
+            client.settings.sessionKey = req.xmldata.session_key;
+            client.settings.uid        = req.xmldata.uid;
+            client.settings.sessionSecret = req.xmldata.secret;
+            dump('session: ' + client.settings.sessionKey + ', uid: ' + client.settings.uid + ', secret: ' + client.settings.sessionSecret + '\n');
+            statusElem.label  = 'logged in!';
+        });
+        statusElem.label = '';
+    } else if (client.settings.authToken) {
+        window.open('http://api.dev005.facebook.com:4750/login.php?api_key=' + client.settings.apiKey +
+                    '&auth_token=' + client.settings.authToken);
+        statusElem.label = 'When you are done, click here';
+        client.settings.loginPending = true;
+    } else {
+        dump('no authToken\n');
+    }
+}
+
+dump('loaded sample.js\n');
