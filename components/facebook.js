ec96804f1e00664a024d86836687d5391f316878
Ari Steinberg <ari@facebook>
2006-10-27 08:32:01 +0000

    rewrite all of the API calls to be asynchronous instead of synchronous.
    this MASSIVELY speeds up the UI (who the fuck knew that all javascript, even inside an XPCOM component, ran in the UI thread?!  actually more relevantly, who the fuck made that decision!?)

--- components/facebook.js
+++ components/facebook.js
@@ -30,6 +30,7 @@ Cc['@mozilla.org/moz/jssubscript-loader;1']
     .getService(Ci.mozIJSSubScriptLoader)
     .loadSubScript('chrome://facebook/content/md5.js');
 
+var fbSvc; // so that all our callback functions objects can access "this"
 function facebookService()
 {
     debug('constructor');
@@ -39,7 +40,7 @@ function facebookService()
 
     this.initValues();
 
-    var fbSvc = this; // so that our timer callback objects can access us
+    fbSvc = this;
     this._msgChecker = {
         notify: function(timer) {
             debug('_msgChecker.notify');
@@ -51,21 +52,17 @@ function facebookService()
     this._friendChecker = {
         notify: function(timer) {
             debug('_friendChecker.notify');
-            fbSvc.checkFriends();
+            fbSvc.checkFriends(false);
         }
     };
     this._initialize = {
         notify: function(timer) {
             debug('_initialize.notify');
             fbSvc.getMyInfo();
-            fbSvc._observerService.notifyObservers(fbSvc._loggedInUser, 'facebook-session-start',
-                                                   fbSvc._loggedInUser.id);
             fbSvc.checkMessages();
             fbSvc.checkPokes();
             fbSvc.checkReqs();
-            fbSvc._holdFriendNotifications = true;
-            fbSvc.checkFriends();
-            fbSvc._holdFriendNotifications = false;
+            fbSvc.checkFriends(true);
         }
     };
     this._ffStart = {
@@ -185,121 +182,137 @@ facebookService.prototype = {
     },
 
     checkMessages: function() {
-        var data = this.callMethod('facebook.messages.getCount', []);
-        var newMsgCount = data.unread;
-        if (data.most_recent > this._lastMsgTime && newMsgCount > 0) {
-            this._observerService.notifyObservers(null, 'facebook-new-msgs', newMsgCount);
-            if (newMsgCount > 1) {
-                this.showPopup('http://static.ak.facebook.com/images/feed_icons/aaron_color/s/poke.gif', 'You have new messages', 'http://www.facebook.com/mailbox.php');
-            } else {
-                this.showPopup('http://static.ak.facebook.com/images/feed_icons/aaron_color/s/poke.gif', 'You have a new message', 'http://www.facebook.com/mailbox.php');
+        this.callMethod('facebook.messages.getCount', [], function(data) {
+            var newMsgCount = data.unread;
+            if (data.most_recent > fbSvc._lastMsgTime && newMsgCount > 0) {
+                fbSvc._observerService.notifyObservers(null, 'facebook-new-msgs', newMsgCount);
+                if (newMsgCount > 1) {
+                    fbSvc.showPopup('http://static.ak.facebook.com/images/feed_icons/aaron_color/s/poke.gif',
+                                    'You have new messages', 'http://www.facebook.com/mailbox.php');
+                } else {
+                    fbSvc.showPopup('http://static.ak.facebook.com/images/feed_icons/aaron_color/s/poke.gif',
+                                    'You have a new message', 'http://www.facebook.com/mailbox.php');
+                }
+                fbSvc._lastMsgTime = data.most_recent;
             }
-            this._lastMsgTime = data.most_recent;
-        }
-        if (newMsgCount != this._numMsgs) {
-            this._observerService.notifyObservers(null, 'facebook-msgs-updated', newMsgCount);
-            this._numMsgs = newMsgCount;
-        }
-        debug('checkMessages: you have ' + this._numMsgs + ' unread messages');
+            if (newMsgCount != fbSvc._numMsgs) {
+                fbSvc._observerService.notifyObservers(null, 'facebook-msgs-updated', newMsgCount);
+                fbSvc._numMsgs = newMsgCount;
+            }
+            debug('checkMessages: you have ' + fbSvc._numMsgs + ' unread messages');
+        });
     },
     checkPokes: function() {
-        var data = this.callMethod('facebook.pokes.getCount', []);
-        var newPokeCount = data.unseen;
-        var totalPokeCount = data.total;
-        if (totalPokeCount > this._totalPokes && newPokeCount > 0) {
-            // we send the notification if you have any unseen pokes and the total # of pokes has gone up.
-            // note that your unseen poke count could theoretically stay the same or even if you have new pokes.
-            this._totalPokes = totalPokeCount;
-            this._observerService.notifyObservers(null, 'facebook-new-poke', newPokeCount);
-            this.showPopup('http://static.ak.facebook.com/images/feed_icons/aaron_color/s/poke.gif', 'You have been poked', 'http://www.facebook.com/home.php');
-        }
-        if (newPokeCount != this._numPokes) {
-            this._numPokes = newPokeCount;
-            this._observerService.notifyObservers(null, 'facebook-pokes-updated', newPokeCount);
-        }
-        debug('checkPokes: you have ' + this._numPokes + ' unseen pokes');
+        this.callMethod('facebook.pokes.getCount', [], function(data) {
+            var newPokeCount = data.unseen;
+            var totalPokeCount = data.total;
+            if (totalPokeCount > fbSvc._totalPokes && newPokeCount > 0) {
+                // we send the notification if you have any unseen pokes and the total # of pokes has gone up.
+                // note that your unseen poke count could theoretically stay the same or even if you have new pokes.
+                fbSvc._totalPokes = totalPokeCount;
+                fbSvc._observerService.notifyObservers(null, 'facebook-new-poke', newPokeCount);
+                fbSvc.showPopup('http://static.ak.facebook.com/images/feed_icons/aaron_color/s/poke.gif',
+                                'You have been poked', 'http://www.facebook.com/home.php');
+            }
+            if (newPokeCount != fbSvc._numPokes) {
+                fbSvc._numPokes = newPokeCount;
+                fbSvc._observerService.notifyObservers(null, 'facebook-pokes-updated', newPokeCount);
+            }
+            debug('checkPokes: you have ' + fbSvc._numPokes + ' unseen pokes');
+        });
     },
     checkReqs: function() {
-        var data = this.callMethod('facebook.friends.getRequests', []);
-        var newReqCount = data.result_elt.length();
-        var reqsToGet = [];
-        for each (var id in data.result_elt) {
-            if (!this._reqsInfo[id]) {
-                reqsToGet.push(id);
-            }
-        }
-        if (reqsToGet.length > 0) {
-            this._reqsInfo = this.getUsersInfo(reqsToGet);
-            for each (var reqInfo in this._reqsInfo) {
-                this._observerService.notifyObservers(reqInfo, 'facebook-new-req', reqInfo['id']);
-                this.showPopup(reqInfo.pic, reqInfo.name + ' wants to be your friend',
-                               'http://www.facebook.com/reqs.php');
-            }
-        }
-        if (newReqCount != this._numReqs) {
-            this._numReqs = newReqCount;
-            this._observerService.notifyObservers(null, 'facebook-reqs-updated', newReqCount);
-        }
-        debug('checkReqs: you have ' + this._numReqs + ' outstanding reqs');
-    },
-    checkFriends: function() {
-        debug('checkFriends');
-        var friendUpdate = false;
-        var data = this.callMethod('facebook.friends.get', []);
-        // make a new friends array every time so that we handle losing friends properly
-        var friends = [];
-        for each (var id in data.result_elt) {
-            friends.push(id);
-        }
-
-        var friendsInfo = this.getUsersInfo(friends);
-        var friendsInfoArr = [];
-
-        for each (var friend in friendsInfo) {
-            if (!this._holdFriendNotifications) {
-                if (!this._friendsInfo[friend['id']]) {
-                    this._observerService.notifyObservers(friend, 'facebook-new-friend', friend['id']);
-                    this.showPopup(friend.pic, friend.name + ' is now your friend',
-                                   'http://www.facebook.com/profile.php?uid=' + friend.id + '&api_key=' + this._apiKey);
-                    friendUpdate = true;
-                } else if (this._friendsInfo[friend['id']].status != friend['status']) {
-                    this._observerService.notifyObservers(friend, 'facebook-friend-updated', friend['id']);
-                    this.showPopup(friend.pic, friend.name + ' is now ' + friend.status,
-                                   'http://www.facebook.com/profile.php?uid=' + friend.id + '&api_key=' + this._apiKey);
-                    friendUpdate = true;
+        this.callMethod('facebook.friends.getRequests', [], function(data) {
+            var newReqCount = data.result_elt.length();
+            var reqsToGet = [];
+            for each (var id in data.result_elt) {
+                if (!fbSvc._reqsInfo[id]) {
+                    reqsToGet.push(id);
                 }
             }
-            friendsInfoArr.push(friend);
-        }
-        this._friendsInfo = friendsInfo;
-        this._friendsInfoArr = friendsInfoArr;
+            if (reqsToGet.length > 0) {
+                fbSvc.getUsersInfo(reqsToGet, function(users) {
+                    fbSvc._reqsInfo = users;
+                    for each (var reqInfo in fbSvc._reqsInfo) {
+                        fbSvc._observerService.notifyObservers(reqInfo, 'facebook-new-req', reqInfo['id']);
+                        fbSvc.showPopup(reqInfo.pic, reqInfo.name + ' wants to be your friend',
+                                       'http://www.facebook.com/reqs.php');
+                    }
+                });
+            }
+            if (newReqCount != fbSvc._numReqs) {
+                fbSvc._numReqs = newReqCount;
+                fbSvc._observerService.notifyObservers(null, 'facebook-reqs-updated', newReqCount);
+            }
+            debug('checkReqs: you have ' + fbSvc._numReqs + ' outstanding reqs');
+        });
+    },
+    checkFriends: function(holdFriendNotifications) {
+        debug('checkFriends');
+        var friendUpdate = false;
+        this.callMethod('facebook.friends.get', [], function(data) {
+            // make a new friends array every time so that we handle losing friends properly
+            var friends = [];
+            for each (var id in data.result_elt) {
+                friends.push(id);
+            }
 
-        if (this._holdFriendNotifications || friendUpdate) {
-            debug('sending notification');
-            this._observerService.notifyObservers(null, 'facebook-friends-updated', null);
-        }
-        debug('done checkFriends', friendUpdate);
+            fbSvc.getUsersInfo(friends, function(friendsInfo) {
+                var friendsInfoArr = [];
+
+                for each (var friend in friendsInfo) {
+                    if (!holdFriendNotifications) {
+                        if (!fbSvc._friendsInfo[friend['id']]) {
+                            fbSvc._observerService.notifyObservers(friend, 'facebook-new-friend', friend['id']);
+                            fbSvc.showPopup(friend.pic, friend.name + ' is now your friend',
+                                            'http://www.facebook.com/profile.php?uid=' + friend.id + '&api_key=' + fbSvc._apiKey);
+                            friendUpdate = true;
+                        } else if (fbSvc._friendsInfo[friend['id']].status != friend['status']) {
+                            fbSvc._observerService.notifyObservers(friend, 'facebook-friend-updated', friend['id']);
+                            fbSvc.showPopup(friend.pic, friend.name + ' is now ' + friend.status,
+                                            'http://www.facebook.com/profile.php?uid=' + friend.id + '&api_key=' + fbSvc._apiKey);
+                            friendUpdate = true;
+                        }
+                    }
+                    friendsInfoArr.push(friend);
+                }
+                fbSvc._friendsInfo = friendsInfo;
+                fbSvc._friendsInfoArr = friendsInfoArr;
+
+                if (holdFriendNotifications || friendUpdate) {
+                    debug('sending notification');
+                    fbSvc._observerService.notifyObservers(null, 'facebook-friends-updated', null);
+                }
+                debug('done checkFriends', friendUpdate);
+            });
+        });
     },
     getFriends: function(count) {
         count.value = this._friendsInfoArr.length;
         return this._friendsInfoArr;
     },
     getMyInfo: function() {
-        this._loggedInUser = this.getUsersInfo([this._uid])[this._uid];
-        debug('getMyInfo: hello', this._loggedInUser['name']);
+        this.getUsersInfo([this._uid], function(users) {
+            fbSvc._loggedInUser = users[fbSvc._uid];
+            fbSvc._observerService.notifyObservers(fbSvc._loggedInUser, 'facebook-session-start',
+                                                   fbSvc._loggedInUser.id);
+            debug('getMyInfo: hello', fbSvc._loggedInUser['name']);
+        });
     },
-    getUsersInfo: function(users) {
-        var data = this.callMethod('facebook.users.getInfo', ['users='+users.join(','), 'fields=name,status,pic']);
-        var usersInfo = {};
-        for each (var user in data.result_elt) {
-            var name   = String(user.name),
-                id     = String(user.@id),
-                status = String(user.status.message),
-                stime  = parseInt(user.status.time),
-                pic    = String(decodeURI(user.pic));
-            usersInfo[id] = new facebookUser(id, name, pic, status, stime);
-        }
-        return usersInfo;
+    getUsersInfo: function(users, callback) {
+        this.callMethod('facebook.users.getInfo', ['users='+users.join(','), 'fields=name,status,pic'],
+                        function(data) {
+            var usersInfo = {};
+            for each (var user in data.result_elt) {
+                var name   = String(user.name),
+                    id     = String(user.@id),
+                    status = String(user.status.message),
+                    stime  = parseInt(user.status.time),
+                    pic    = String(decodeURI(user.pic));
+                usersInfo[id] = new facebookUser(id, name, pic, status, stime);
+            }
+            callback(usersInfo);
+        });
     },
 
     generateSig: function (params) {
@@ -315,7 +328,7 @@ facebookService.prototype = {
     // functions - ie things other than facebook.auth.*.  The login-related
     // calls are done in the chrome layer.
     // Also note that this is synchronous so you should not call it from the UI.
-    callMethod: function (method, params) {
+    callMethod: function (method, params, callback) {
         if (!this._loggedIn) return null;
 
         params.push('method=' + method);
@@ -337,28 +350,39 @@ facebookService.prototype = {
             channel.QueryInterface(Ci.nsIUploadChannel)
                    .setUploadStream(upStream, "application/x-www-form-urlencoded", -1);
             channel.requestMethod = "POST";
-            var downStream = channel.open();
-            var sis = Cc["@mozilla.org/scriptableinputstream;1"].createInstance(Ci.nsIScriptableInputStream);
-            sis.init(downStream);
-            var txt, resultText = '';
-            while (txt = sis.read(-1)) {
-                resultText += txt;
-            }
-            resultText = resultText.substr(resultText.indexOf("\n") + 1);
-            if (VERBOSITY == 2) {
-              debug('received text:');
-              dump(resultText);
-            }
-            var xmldata = new XML(resultText);
-            if ((String)(xmldata.fb_error.code)) { // need to cast to string or else the check will never fail
-                throw xmldata.fb_error;
-            }
-            return xmldata;
+            var listener = {
+                onDataAvailable: function(request, context, inputStream, offset, count) {
+                    var sis = Cc["@mozilla.org/scriptableinputstream;1"].createInstance(Ci.nsIScriptableInputStream);
+                    sis.init(inputStream);
+                    this.resultTxt += sis.read(count);
+                },
+                onStartRequest: function(request, context) {
+                    this.resultTxt = '';
+                },
+                onStopRequest: function(request, context, statusCode) {
+                    if (statusCode == Components.results.NS_OK) {
+                        this.resultTxt = this.resultTxt.substr(this.resultTxt.indexOf("\n") + 1);
+                        if (VERBOSITY == 2) {
+                          debug('received text:');
+                          dump(this.resultTxt);
+                        }
+                        var xmldata = new XML(this.resultTxt);
+                        if ((String)(xmldata.fb_error.code)) { // need to cast to string or check will never fail
+                            if (xmldata.fb_error.code == 102) {
+                                debug('session expired, logging out.');
+                                fbSvc.sessionEnd();
+                            } else {
+                                debug('API error:');
+                                dump(xmldata.fb_error);
+                            }
+                        } else {
+                            callback(xmldata);
+                        }
+                    }
+                }
+            };
+            channel.asyncOpen(listener, null);
         } catch (e) {
-            if (e.code == 102) {
-                debug('session expired, logging out.');
-                this.sessionEnd();
-            }
             debug('Exception sending REST request: ', e);
             return null;
         }
