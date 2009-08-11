77876dc3703da4872760284aa6f08c53193c507c
Eugene Letuchy <eugene@facebook>
2009-08-10 19:53:18 -0700

    removing build.bat ... superceded by Makefile

--- build.bat
+++
@@ -1,4 +0,0 @@
-name=../facebook_firefox-1.3.xpi
-rm -fv $name
-zip -r $name chrome chrome.manifest components defaults install.rdf license.txt -x "*.svn*" -x "*.bat" -x "*~" -x "*.kpf";
-echo $name;
